58d1390eda8bf5b886e3f73f6028194306bcb817
fuXianLin <402240265@qq.com>
2022-06-18 18:54:50 +0800

    finish first work

---
+++ src/components/FruitStore.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="fruit">
+    <ul>
+      <li>欢迎光临我们的水果店</li>
+      <li>苹果10￥/斤，折扣(8折)</li>
+      <li>请输入要购买的斤数 <input type="text" v-model="num" /></li>
+      <li>
+        <button @click="checkOut">结账买单~</button>
+      </li>
+      <li>
+        结账单：总价： {{ total }}￥ 折后价： {{ discount }}￥ 省了：
+        {{ saveMoney }}￥
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FruitStore",
+  data() {
+    return {
+      num: 1,
+    };
+  },
+  computed: {
+    total() {
+      return this.num * 10;
+    },
+    discount() {
+      return (this.total / 10) * 8;
+    },
+    saveMoney() {
+      return this.total - this.discount;
+    },
+  },
+  methods: {
+    checkOut() {
+      this.num = 1;
+      alert("结账成功，欢迎再次光临");
+    },
+  },
+};
+</script>
+
+<style scope>
+* {
+  margin: 0;
+  padding: 0;
+}
+.fruit {
+  /* width: 500px; */
+  height: 300px;
+  margin: 0 auto;
+}
+.fruit ul li {
+  list-style: none;
+  height: 60px;
+  line-height: 60px;
+  text-align: center;
+  border: 1px solid #999;
+}
+</style>
---
+++ src/components/LikeMovieType.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="LikeMovieType">
+    <p>请选择喜欢的专栏</p>
+    <ol>
+      <li v-for="(h, index) in hoppys" :key="index">
+        <input type="checkbox" v-model="flag" :value="h"/>{{ h }}
+      </li>
+    </ol>
+    <ul>
+      <li v-for="(h, index) in flag" :key="index">
+        {{ h }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LikeMovieType",
+  data() {
+    return {
+      hoppys: ["科幻", "喜剧", "动漫", "冒险", "科技", "军事", "娱乐", "奇闻"],
+      flag: [],
+    };
+  },
+};
+</script>
+
+<style scope>
+.LikeMovieType {
+  margin: 50px auto;
+  text-align: center;
+}
+
+ol {
+  display: flex;
+  justify-content: center;
+  margin-top: 50px;
+}
+ol li {
+  list-style: none;
+  margin: 0 10px;
+}
+ul {
+  margin-top: 50px;
+}
+ul li {
+  margin: 10px;
+}
+</style>
