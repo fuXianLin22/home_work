dbe064289368bd179e6be8e90d904930c2c2ccd6
fuxianlin <402240265@qq.com>
2023-01-08 19:32:33 +0800

    主页添加了标题

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <h1 class="title">hello world</h1>
     <FruitStore></FruitStore>
     <!-- <hr /> -->
     <LikeMovieType></LikeMovieType>
@@ -12,9 +13,14 @@ import FruitStore from "./components/FruitStore.vue";
 
 export default {
   name: "App",
-  components: { FruitStore, LikeMovieType },
+  components: {FruitStore, LikeMovieType},
 };
 </script>
 
-<style>
+<style scoped>
+.title {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
 </style>
