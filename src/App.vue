4a108a8b20c0d78c6bf6e2b85c02545504c947eb
fuxianlin <402240265@qq.com>
2023-01-09 20:24:12 +0800

    对window.open方法简单的试验了一下

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <h1 class="title">hello world</h1>
+    <button @click="btn">链接</button>
     <FruitStore></FruitStore>
     <!-- <hr /> -->
     <LikeMovieType></LikeMovieType>
@@ -14,6 +15,12 @@ import FruitStore from "./components/FruitStore.vue";
 export default {
   name: "App",
   components: {FruitStore, LikeMovieType},
+  methods: {
+    btn() {
+      // window.open('http://10.73.194.156:18099/api/download/u=763387957,687177997&fm=253&fmt=auto&app=138&f=JPEG_20230109173657.png', '_blank')
+      window.open('https://www.baidu.com')
+    }
+  }
 };
 </script>
 
